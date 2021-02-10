4bc8796dfc11aeedd81faf0feda9f113ab0008c5
aomine <aomine1745@gmail.com>
2021-02-10 00:29:02 -0500

    v3.0

--- js/app.js
+++ js/app.js
@@ -3,18 +3,18 @@ const switchTheme = document.getElementById("switch-theme");
 switchTheme.addEventListener("click", handleSwitchTheme);
 
 function handleSwitchTheme() {
-  if (document.body.classList.contains("dark-theme")) {
-    document.body.classList.replace("dark-theme", "light-theme");
+  if (document.body.dataset.theme === "dark") {
+    document.body.dataset.theme = "light";
     switchTheme.innerHTML = "Dark";
   } else {
-    document.body.classList.replace("light-theme", "dark-theme");
+    document.body.dataset.theme = "dark";
     switchTheme.innerHTML = "Light";
   }
 }
 
 if (window.matchMedia("(prefers-color-scheme)").media !== "not all") {
   if (matchMedia("(prefers-color-scheme: dark)").matches) {
-    document.body.classList.replace("light-theme", "dark-theme");
+    document.body.dataset.theme = "dark";
     switchTheme.innerHTML = "Light";
   }
 }
