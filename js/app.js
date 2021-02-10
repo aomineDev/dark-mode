19f01f0bf662d4264bc8d343f175dbc9dcb61822
aomine <aomine1745@gmail.com>
2021-02-09 20:17:21 -0500

    v2.0

--- js/app.js
+++ js/app.js
@@ -3,13 +3,18 @@ const switchTheme = document.getElementById("switch-theme");
 switchTheme.addEventListener("click", handleSwitchTheme);
 
 function handleSwitchTheme() {
-  const body = document.body;
-
-  if (body.classList.contains("dark-theme")) {
-    body.classList.replace("dark-theme", "light-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    document.body.classList.replace("dark-theme", "light-theme");
     switchTheme.innerHTML = "Dark";
   } else {
-    body.classList.replace("light-theme", "dark-theme");
+    document.body.classList.replace("light-theme", "dark-theme");
+    switchTheme.innerHTML = "Light";
+  }
+}
+
+if (window.matchMedia("(prefers-color-scheme)").media !== "not all") {
+  if (matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.replace("light-theme", "dark-theme");
     switchTheme.innerHTML = "Light";
   }
 }
